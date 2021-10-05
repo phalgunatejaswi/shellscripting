7b420895d34b769fda1a20df51d0049f82f76ca4
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-06 04:30:28 +0530

    Added Variables demo script

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#Static Variable
+A=15
+echo $A
+
+# Providing variable content dynamically
+
+NO_OF_USERS_CONNECTED = $(who |wc -l)
+echo Number of users connected is $NO_OF_USERS_CONNECTED
