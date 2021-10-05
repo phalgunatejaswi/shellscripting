ae54db0e006195dc9b3d52e69a2f98cf8e404405
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-06 05:27:47 +0530

    Commented the lines in 01-shell.sh file

--- 01-shell.sh
+++ 01-shell.sh
@@ -2,14 +2,14 @@
 
 tail /var/log/messages
 
-She-Bang
+#She-Bang
 #! is called She-Bang
-It denotes the path of interpretor for the remaining lines of the script
-In general, it is always best to put SHe-Bang at the start of the script.
-Only one She-Bang in a single file
+#It denotes the path of interpreter for the remaining lines of the script
+#In general, it is always best to put SHe-Bang at the start of the script.
+#Only one She-Bang in a single file
 
-In Redhat family, both shell and bash shell are same
-But, it is different for Ubuntu and other families
+#In Redhat family, both shell and bash shell are same
+#But, it is different for Ubuntu and other families
 
-Comments:
-To comment a line, use # at the start of the line.
+#Comments:
+#To comment a line, use # at the start of the line.
