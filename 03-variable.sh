855583c1b44d658d4ddb8bb81edc87295b30949c
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-06 04:49:01 +0530

    Added Dynamic variables with current date and time example

--- 03-variable.sh
+++ 03-variable.sh
@@ -9,3 +9,5 @@ echo Dynamic Variale
 NO_OF_USERS_CONNECTED=$(who |wc -l)
 echo Number of users connected is $NO_OF_USERS_CONNECTED
 
+DATE_TIME=$(date +"%D %T")
+echo current date and time is $DATE_TIME
