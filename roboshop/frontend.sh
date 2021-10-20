78702a2a1b7042fe1a0eef9a0f0f2f19de072250
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-20 23:39:25 +0530

    updated comments in frontend.sh

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,13 +2,16 @@
 
 LOG=/tmp/roboshop.log
 rm -rf $LOG
-echo -e "Installing nginx\t\t...\t\e[33mdone\e[0m"
+echo -e "Installing nginx"
 yum install nginx -y &>>$LOG
+echo -e "Installed nginx\t\t...\t\e[33mdone\e[0m"
 
-echo "Enable nginx service"
+echo "Enabling nginx service"
 systemctl enable nginx &>>$LOG
+echo "Enabled nginx service\t\t...\t\e[33mdone\e[0m"
 
 echo "starting nginx"
 systemctl start nginx &>>$LOG
+echo "started nginx\t\t...\t\e[33mdone\e[0m"
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
