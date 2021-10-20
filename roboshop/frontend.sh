c538bd4e7b111991fe9664dfa65572ad271306c1
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-21 00:50:39 +0530

    added a validate function to validate the commands are run successfully or not and to keep the code dry.

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,31 +2,26 @@
 
 LOG=/tmp/roboshop.log
 rm -rf $LOG
+
+validate() {
+  if [ "$1" -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m failed\e[0m"
+    exit 1
+  fi
+}
+
 echo -n -e "Installing nginx\t\t..."
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m failed\e[0m"
-  exit 1
-fi
+validate $?
 
 echo -n -e "Enabling nginx service\t\t..."
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m failed\e[0m"
-  exit 1
-fi
+validate $?
 
 echo -n -e "starting nginx service\t\t..."
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m failed\e[0m"
-  exit 1
-fi
+validate $?
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
