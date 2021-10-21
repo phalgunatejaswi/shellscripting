9cdaf9755b25b8be68b25eb03ec0253762887b75
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-22 03:09:01 +0530

    Updated the mongodb.sh script

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-PRINT "Setting up MongoDB Repository"
+PRINT "Setting up MongoDB Repository\t"
 echo '[mongodb-org-5.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
@@ -11,7 +11,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc' >/etc/yum.repos.d/mongodb.repo
 VALIDATE $?
 
-PRINT "Install MongoDB\t"
+PRINT "Install MongoDB\t\t\t"
 yum install -y mongodb-org &>>"$LOG"
 VALIDATE $?
 
@@ -23,10 +23,10 @@ PRINT "Enable and Start MongoDB Service"
 systemctl enable mongod && systemctl start mongod &>>"$LOG"
 VALIDATE $?
 
-PRINT "Download MongoDB Schema"
+PRINT "Download MongoDB Schema\t\t"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>"$LOG"
 VALIDATE $?
 
 PRINT "Load MongoDB Schema"
-cd /tmp && unzip -o mongodb.zip && cd mongodb-main && mongo < catalogue.js && mongo < users.js
+cd /tmp &>>"$LOG" && unzip -o mongodb.zip &>>"$LOG" && cd mongodb-main &>>"$LOG" && mongo < catalogue.js &>>"$LOG" && mongo < users.js &>>"$LOG"
 VALIDATE $?
