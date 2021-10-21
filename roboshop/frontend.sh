9ec3e4b28bd881e472da90650642acd1adff2cc0
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-22 02:02:48 +0530

    Completed the Frontend script

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,12 +6,27 @@ PRINT "Installing nginx"
 yum install nginx -y &>>"$LOG"
 VALIDATE $?
 
+PRINT "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>"$LOG"
+VALIDATE $?
+
+PRINT "Remove Old Htdocs"
+cd /usr/share/nginx/html && rm -rf * &>>"$LOG"
+VALIDATE $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>"$LOG" && mv frontend-main/* . &>>"$LOG" && mv static/* . &>>"$LOG" && rm -rf frontend-master static &>>"$LOG"
+VALIDATE $?
+
+PRINT "Update RoboShop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>>"$LOG"
+VALIDATE $?
+
 PRINT "Enabling nginx service"
 systemctl enable nginx &>>"$LOG"
 VALIDATE $?
 
 PRINT "starting nginx service"
-systemctl start nginx &>>"$LOG"
+systemctl restart nginx &>>"$LOG"
 VALIDATE $?
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
