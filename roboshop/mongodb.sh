a60e74c537f1eb9e6e9de1e19e6ef49dc7ae04b6
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-22 03:05:59 +0530

    Updated the mongodb.sh script

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -3,16 +3,16 @@
 source common.sh
 
 PRINT "Setting up MongoDB Repository"
-echo "[mongodb-org-5.0]
+echo '[mongodb-org-5.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc" >/etc/yum.repos.d/mongodb.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc' >/etc/yum.repos.d/mongodb.repo
 VALIDATE $?
 
 PRINT "Install MongoDB\t"
-sudo yum install -y mongodb-org &>>"$LOG"
+yum install -y mongodb-org &>>"$LOG"
 VALIDATE $?
 
 PRINT "Updating MongoDB Listener Address"
