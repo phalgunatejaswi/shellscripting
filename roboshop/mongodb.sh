3cde9141a8a3437a4996b0ab06051e41d2f90762
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-22 02:57:34 +0530

    Updated the mongodb.sh script

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -3,9 +3,9 @@
 source common.sh
 
 PRINT "Setting up MongoDB Repository"
-echo "mongodb-org-4.2]
+echo "[mongodb-org-4.2]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/8Server/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc" >/etc/yum.repos.d/mongodb.repo
