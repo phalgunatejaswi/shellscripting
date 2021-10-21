37be2b1f82745c156df1a0ae669d5de10ddee322
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-22 01:37:12 +0530

    added common.sh for commonly used functions

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,15 +3,15 @@
 source common.sh
 
 print "Installing nginx"
-yum install nginx -y &>>$LOG
+yum install nginx -y &>>"$LOG"
 validate $?
 
 print "Enabling nginx service"
-systemctl enable nginx &>>$LOG
+systemctl enable nginx &>>"$LOG"
 validate $?
 
 print "starting nginx service"
-systemctl start nginx &>>$LOG
+systemctl start nginx &>>"$LOG"
 validate $?
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
