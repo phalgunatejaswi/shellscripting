c51377e1f05f4f5eafd4c22e3c497c6dee30fceb
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-23 11:59:32 +0530

    added reset mysql password code block to mysql.sh

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -19,7 +19,7 @@ systemctl enable mysqld &>>"$LOG" && systemctl start mysqld  &>>"$LOG"
 VALIDATE $?
 
 PRINT "Reset MySQL root password"
-$DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{printf $NF}')
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{printf $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Robosho@123';" | mysql -uroot -p"$DEFAULT_PASSWORD" &>>"$LOG"
 #Next, We need to change the default root password in order to start using the database service.
 ## mysql_secure_installation
