4295b8f5d86828b39d528b5190fd9541161cd9bd
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-23 10:16:26 +0530

    Created a function called NODEJS in common.sh to keep the code dry and use that function in both catalogue.sh and user.sh

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -2,37 +2,40 @@
 
 source common.sh
 
-PRINT "Install NodeJS\t\t"
-yum install nodejs make gcc-c++ -y &>>"$LOG"
-VALIDATE $?
+COMPONENT=catalogue
+NODEJS
 
-PRINT "Add Application User\t"
-id roboshop &>>"$LOG"
-if [ $? -ne 0 ]; then
-  useradd roboshop &>>"$LOG"
-fi
-VALIDATE $?
-
-PRINT "Download Application Code"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>"$LOG"
-VALIDATE $?
-
-PRINT "Extract Application Code"
-cd /home/roboshop &>>"$LOG" && unzip /tmp/catalogue.zip &>>"$LOG" && rm -rf catalogue &>>"$LOG" && mv catalogue-main catalogue &>>"$LOG"
-VALIDATE $?
-
-PRINT "Install NodeJS Dependencies"
-cd /home/roboshop/catalogue &>>"$LOG" && npm install --unsafe-perm &>>"$LOG"
-VALIDATE $?
-
-PRINT "Fix App User Permissions"
-chown roboshop:roboshop /home/roboshop -R &>>"$LOG"
-VALIDATE $?
-
-PRINT "Update SystemD file\t"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>"$LOG" && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>"$LOG"
-VALIDATE $?
-
-PRINT "Start Catalogue Service\t"
-systemctl daemon-reload &>>"$LOG" && systemctl start catalogue &>>"$LOG" && systemctl enable catalogue &>>"$LOG"
-VALIDATE $?
+#PRINT "Install NodeJS\t\t"
+#yum install nodejs make gcc-c++ -y &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Add Application User\t"
+#id roboshop &>>"$LOG"
+#if [ $? -ne 0 ]; then
+#  useradd roboshop &>>"$LOG"
+#fi
+#VALIDATE $?
+#
+#PRINT "Download Application Code"
+#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Extract Application Code"
+#cd /home/roboshop &>>"$LOG" && unzip /tmp/catalogue.zip &>>"$LOG" && rm -rf catalogue &>>"$LOG" && mv catalogue-main catalogue &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Install NodeJS Dependencies"
+#cd /home/roboshop/catalogue &>>"$LOG" && npm install --unsafe-perm &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Fix App User Permissions"
+#chown roboshop:roboshop /home/roboshop -R &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Update SystemD file\t"
+#sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>"$LOG" && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>"$LOG"
+#VALIDATE $?
+#
+#PRINT "Start Catalogue Service\t"
+#systemctl daemon-reload &>>"$LOG" && systemctl start catalogue &>>"$LOG" && systemctl enable catalogue &>>"$LOG"
+#VALIDATE $?
--- roboshop/common.sh
+++ roboshop/common.sh
@@ -24,3 +24,39 @@ PRINT() {
   echo -n -e "$1\t\t..."
 }
 
+NODEJS() {
+  PRINT "Install NodeJS\t\t"
+  yum install nodejs make gcc-c++ -y &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Add Application User\t"
+  id roboshop &>>"$LOG"
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>"$LOG"
+  fi
+  VALIDATE $?
+
+  PRINT "Download Application Code"
+  curl -s -L -o /tmp/{COMPONENT}.zip "https://github.com/roboshop-devops-project/{COMPONENT}/archive/main.zip" &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Extract Application Code"
+  cd /home/roboshop &>>"$LOG" && unzip /tmp/{COMPONENT}.zip &>>"$LOG" && rm -rf {COMPONENT} &>>"$LOG" && mv {COMPONENT}-main {COMPONENT} &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Install NodeJS Dependencies"
+  cd /home/roboshop/{COMPONENT} &>>"$LOG" && npm install --unsafe-perm &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Fix App User Permissions"
+  chown roboshop:roboshop /home/roboshop -R &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Update SystemD file\t"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/{COMPONENT}/systemd.service &>>"$LOG" && mv /home/roboshop/{COMPONENT}/systemd.service /etc/systemd/system/{COMPONENT}.service &>>"$LOG"
+  VALIDATE $?
+
+  PRINT "Start {COMPONENT} Service\t"
+  systemctl daemon-reload &>>"$LOG" && systemctl start {COMPONENT} &>>"$LOG" && systemctl enable {COMPONENT} &>>"$LOG"
+  VALIDATE $?
+}
