2ff0c989e1098de798f61d9069c9d6a9cb8e7090
phalgunatejaswi <phalgunatejaswi@gmail.com>
2021-10-25 01:15:34 +0530

    Added new functions for removing the repetitive code in common.sh. Also,finished the Shipping.sh script.

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -23,19 +23,16 @@ PRINT() {
   echo -e "=============================\t$1\t=============================" &>>"$LOG"
   echo -n -e "$1\t\t..."
 }
-
-NODEJS() {
-  PRINT "Install NodeJS\t\t"
-  yum install nodejs make gcc-c++ -y &>>"$LOG"
-  VALIDATE $?
-
+ADD_APP_USER() {
   PRINT "Add Application User\t"
   id roboshop &>>"$LOG"
   if [ $? -ne 0 ]; then
     useradd roboshop &>>"$LOG"
   fi
   VALIDATE $?
+}
 
+DOWNLOAD_APP_CODE() {
   PRINT "Download Application Code"
   curl -s -L -o /tmp/"${COMPONENT}".zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>"$LOG"
   VALIDATE $?
@@ -44,19 +41,54 @@ NODEJS() {
   cd /home/roboshop &>>"$LOG" && unzip -o /tmp/"${COMPONENT}".zip &>>"$LOG" && rm -rf "${COMPONENT}" &>>"$LOG" && mv "${COMPONENT}"-main "${COMPONENT}" &>>"$LOG"
   VALIDATE $?
 
-  PRINT "Install NodeJS Dependencies"
-  cd /home/roboshop/"${COMPONENT}" &>>"$LOG" && npm install --unsafe-perm &>>"$LOG"
-  VALIDATE $?
+}
 
+FIX_PERMISSIONS() {
   PRINT "Fix App User Permissions"
   chown roboshop:roboshop /home/roboshop -R &>>"$LOG"
   VALIDATE $?
+}
 
+SYSTEMD_SERVICE() {
   PRINT "Update SystemD file\t"
-  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" /home/roboshop/"${COMPONENT}"/systemd.service &>>"$LOG" && mv /home/roboshop/"${COMPONENT}"/systemd.service /etc/systemd/system/"${COMPONENT}".service &>>"$LOG"
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" /home/roboshop/"${COMPONENT}"/systemd.service &>>"$LOG" && mv /home/roboshop/"${COMPONENT}"/systemd.service /etc/systemd/system/"${COMPONENT}".service &>>"$LOG"
   VALIDATE $?
 
   PRINT "Start ${COMPONENT} Service\t"
   systemctl daemon-reload &>>"$LOG" && systemctl restart "${COMPONENT}" &>>"$LOG" && systemctl enable "${COMPONENT}" &>>"$LOG"
   VALIDATE $?
+
+}
+
+NODEJS() {
+  PRINT "Install NodeJS\t\t"
+  yum install nodejs make gcc-c++ -y &>>"$LOG"
+  VALIDATE $?
+
+  ADD_APP_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install NodeJS Dependencies"
+  cd /home/roboshop/"${COMPONENT}" &>>"$LOG" && npm install --unsafe-perm &>>"$LOG"
+  VALIDATE $?
+
+  FIX_PERMISSIONS
+  SYSTEMD_SERVICE
+}
+
+
+MAVEN() {
+  PRINT "Install MAVEN\t\t"
+  um install maven -y &>>"$LOG"
+  VALIDATE $?
+
+  ADD_APP_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Clean Maven Package"
+  cd /home/roboshop/"${COMPONENT}" &>>"$LOG" && mvn clean package --unsafe-perm &>>"$LOG" && mv target/shipping-1.0.jar shipping.jar &>>"$LOG"
+  VALIDATE $?
+
+  FIX_PERMISSIONS
+  SYSTEMD_SERVICE
 }
